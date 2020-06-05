4106be7e1ce56c31922816ca6cf08e055ddf8995
krikalyt@gmail.com <krikalyt@gmail.com>
2020-06-05 11:45:01 +0530

    26 on graph

--- graph/cses-27.cpp
+++ graph/cses-27.cpp
@@ -19,10 +19,11 @@
 using namespace std;
 #define mod 1000000007
 typedef long long int ll;
-int par[200000];
-vector<int> edge[200000];
-vector<int> edge2[200000];
-int indegree[200000] = {0};
+int par[100000];
+vector<int> edge[100000];
+vector<int> edge2[100000];
+ll v_weight[100000];
+set<int> edge3[100000];
 void dfs(int s,vector<int> edge[],bool visited[],stack<int>& st){
     visited[s] = true;
     for(int x : edge[s])
@@ -54,6 +55,84 @@ int main(int size,char** args)
     std::cin.tie(NULL);
     int n,e;
     cin >> n >> e;
+    for(int i=0;i<n;i++)
+        cin >> v_weight[i];
+    
+    for(int i=0;i<e;i++){
+        int a,b;
+        cin >> a >> b;
+        a--;b--;
+        edge[a].push_back(b);
+        edge2[b].push_back(a);
+    }
 
+    //kosaraju 
+    stack<int> st;
+    bool visited[n] = {false};
+    for(int i=0;i<n;i++){
+        if(visited[i] == false){
+            dfs(i,edge,visited,st);
+        }
+    }
+
+    int i = 0;
+    bool possible = true;
+    fill(visited,visited+n,false);
+    while(st.size() > 0){
+        if(visited[st.top()]){
+            st.pop();
+            continue;
+        }
+        dfs2(st.top(),edge2,visited,i);
+        i++;
+        st.pop();
+    }
+    int e3s = i;
+    int indeg[e3s] = {0};
+    ll dist[e3s] = {0};
+    ll dist2[e3s] = {0};
+    for(int i=0;i<n;i++){
+        dist[par[i]] += v_weight[i];
+    }
+
+    for(int i=0;i<n;i++){
+        for(int x : edge[i])
+        {
+            if(par[x] == par[i])
+                continue;
+            if(edge3[par[i]].find(par[x]) == edge3[par[i]].end()){
+                edge3[par[i]].insert(par[x]);
+                indeg[par[x]]++;
+            }
+        }
+    }
+
+    //topological_sort max dist 
+    queue<int> abc;
+    for(int i=0;i<e3s;i++){
+        if(indeg[i] == 0){
+            abc.push(i);
+            dist2[i] = dist[i];
+        }
+    }
+
+    while(abc.size()){
+        int node = abc.front();
+        abc.pop();
+        for(int x : edge3[node]){
+            indeg[x]--;
+            if(dist2[x] < dist2[node] + dist[x]){
+                dist2[x] = dist2[node] + dist[x];
+            }
+            if(indeg[x] == 0){
+                abc.push(x);
+            }
+        }
+    }
+    ll maxe = 0;
+    for(int i=0;i<e3s;i++){
+        maxe = max(maxe,dist2[i]);
+    }
+    cout << maxe << '\n';
     return 0;
 }
