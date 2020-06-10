40f86a0edce04c214b2231d6f8dfe8f180e672df
krikalyt@gmail.com <krikalyt@gmail.com>
2020-06-10 11:24:43 +0530

    dp 12

--- Dynamic Programming/cses-12.cpp
+++ Dynamic Programming/cses-12.cpp
@@ -19,6 +19,54 @@
 using namespace std;
 #define mod 1000000007
 typedef long long int ll;
+long long int result[2][5000][5000];
+bool done[5000][5000];
+pair<ll,ll> solve(int i,int j,bool first,int arr[]){
+    if(i == j && first){
+        return {arr[i],0};
+    }
+
+    if(i == j && first == false){
+        return {0,arr[i]};
+    }
+    if(done[i][j]){
+        if(first){
+            return {result[0][i][j],result[1][i][j]};
+        }else{
+            return {result[1][i][j],result[0][i][j]};
+        }
+    }
+
+    pair<ll,ll> temp = solve(i+1,j,!first,arr);
+    pair<ll,ll> temp2 = solve(i,j-1,!first,arr);
+    pair<ll,ll> temp3;
+    if(first == true){
+        ll data1 = temp.first;
+        ll data2 = temp2.first;
+        if(data1 + arr[i] >= data2 + arr[j]){
+            temp3 = {data1 + arr[i],temp.second};
+        }else{
+            temp3 = {data2 + arr[j],temp2.second};
+        }
+    }else{
+        ll data1 = temp.second;
+        ll data2 = temp2.second;
+        if(data1 + arr[i] >= data2 + arr[j]){
+            temp3 = {temp.first,data1 + arr[i]};
+        }else{
+            temp3 = {temp2.first,data2 + arr[j]};
+        }
+    }
+    done[i][j] = true;
+    if(first){
+        result[0][i][j] = temp3.first;
+        result[1][i][j] = temp3.second;
+    }else{
+        result[1][i][j] = temp3.first;
+        result[0][i][j] = temp3.second;
+    }
+    return temp3;
+}
 int main(int size,char** args)
 {
     // basic input output preset
@@ -31,5 +79,14 @@ int main(int size,char** args)
     }
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    pair<ll,ll> ans = solve(0,n-1,true,arr);
+    cout << ans.first << '\n';
     return 0;
 }
