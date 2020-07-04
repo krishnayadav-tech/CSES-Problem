6104e2b86f98dc28f900bae96ec8589bded95ab8
krikalyt@gmail.com <krikalyt@gmail.com>
2020-07-04 21:52:45 +0530

    minimal-rotation

--- String Algorithm/cses-05.cpp
+++ String Algorithm/cses-05.cpp
@@ -23,6 +23,32 @@ using namespace std;
 typedef long long int ll;
 typedef pair<int,int> pi;
 
+
+int Least_Rotation(string S){
+    S = S + S;
+    int f[S.size()];
+    fill(f,f+S.size(),-1);
+    int k = 0;
+    for(int j=1;j<S.size();j++){
+        int sj = S[j];
+        int i = f[j - k - 1];
+        while((i != -1) and (sj != S[k + i + 1])){
+            if(sj < S[k + i + 1])
+                k = j - i - 1;
+            i = f[i];
+        }
+        if (sj != S[k + i + 1]){
+            if (sj < S[k])
+                k = j;
+            f[j - k] = -1;
+        }
+        else{
+            f[j - k] = i + 1;
+        }
+    }
+    return k;
+}
+
 int main(int size,char** args)
 {
     // basic input output preset
@@ -30,10 +56,20 @@ int main(int size,char** args)
         string args2 = args[1];
         if(args2 == "-onlinejudge"){
             freopen("input.txt","r",stdin);
-            // freopen("output.txt","w",stdout);
+            freopen("output.txt","w",stdout);
         }
     }
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);   
+    string a;
+    cin >> a;
+    int index = Least_Rotation(a);
+    for(int i=index;i<a.size();i++){
+        cout << a[i];
+    }
+    for(int i=0;i<index;i++){
+        cout << a[i];
+    }
+    cout << '\n';
     return 0;
 }
